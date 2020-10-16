1bb870551e09fd8142ec5dc0f34ccfd40b70265b
mzygor <michal@winami.com>
2020-10-16 09:19:50 +0200

    005_ex_3: correction. Thank you for tips. Now code is shorter and better reading.

--- src/YearCheck.java
+++ src/YearCheck.java
@@ -1,13 +1,8 @@
 public class YearCheck {
-    private boolean resultMod4;
-    private boolean resultMod100;
-    private boolean resultMod400;
-
     public boolean isLeap(int year){
-        this.resultMod4 = (year % 4 == 0);
-        this.resultMod100 = (year % 100 == 0);
-        this.resultMod400 = (year % 400 == 0);
-        return ((this.resultMod4 && !this.resultMod100) || this.resultMod400);
+        boolean resultMod4 = (year % 4 == 0);
+        boolean resultMod100 = (year % 100 == 0);
+        boolean resultMod400 = (year % 400 == 0);
+        return ((resultMod4 && !resultMod100) || resultMod400);
     }
-
 }
